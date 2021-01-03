cc655fe54e12bb69e41d807e1dd351ea9500d6ce
Niraj-Prasad-Yadav <yadavniraj898@gmail.com>
2021-01-03 12:39:58 +0530

    Induction process update

--- js/custom.js
+++ js/custom.js
@@ -201,3 +201,20 @@ jQuery(function($) {
 	counterInit();
   
 });
+(function ($) {
+	var speed = 5000;
+	var container = $('.feature-itens');
+	container.each(function () {
+		var elements = $(this).children();
+		elements.each(function () {
+			var elementOffset = $(this).offset();
+			var offset = elementOffset.left * 0.8 + elementOffset.top;
+			var delay = parseFloat(offset / speed).toFixed(2);
+			$(this)
+				.css("-webkit-transition-delay", delay + 's')
+				.css("-o-transition-delay", delay + 's')
+				.css("transition-delay", delay + 's')
+				.addClass('animated');
+		});
+	});
+})(jQuery);
